ff838e2ffc8508d5fc14b9fd5f3f8d53f28decc7
madrilene <hola@lenesaile.com>
2024-05-20 10:56:33 +0200

    back to referenced css file

--- src/_config/plugins/css-config.js
+++ src/_config/plugins/css-config.js
@@ -4,8 +4,8 @@ import postcssImportExtGlob from 'postcss-import-ext-glob';
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 import cssnano from 'cssnano';
-import fs from 'node:fs/promises';
-import path from 'node:path';
+
+// CSS and JavaScript as first-class citizens in Eleventy: https://pepelsbey.dev/articles/eleventy-css-js/
 
 export const cssConfig = eleventyConfig => {
   eleventyConfig.addTemplateFormats('css');
@@ -13,30 +13,24 @@ export const cssConfig = eleventyConfig => {
   eleventyConfig.addExtension('css', {
     outputFileExtension: 'css',
     compile: async (inputContent, inputPath) => {
-      let outputPath;
-
-      if (inputPath.endsWith('/src/assets/css/global.css')) {
-        outputPath = './src/_includes/css/global-inline.css';
-      } else if (inputPath.includes('/src/assets/css/components/')) {
-        const filename = path.basename(inputPath);
-        outputPath = `./dist/assets/components/${filename}`;
-      } else {
-        return () => '';
+      if (
+        !inputPath.endsWith('/src/assets/css/global.css') &&
+        !inputPath.includes('/src/assets/css/components/')
+      ) {
+        return;
       }
 
-      const result = await postcss([
-        postcssImportExtGlob,
-        postcssImport,
-        tailwindcss,
-        autoprefixer,
-        cssnano
-      ]).process(inputContent, {from: inputPath});
+      return async () => {
+        let result = await postcss([
+          postcssImportExtGlob,
+          postcssImport,
+          tailwindcss,
+          autoprefixer,
+          cssnano
+        ]).process(inputContent, {from: inputPath});
 
-      await fs.mkdir(path.dirname(outputPath), {recursive: true});
-
-      await fs.writeFile(outputPath, result.css);
-
-      return () => result.css;
+        return result.css;
+      };
     }
   });
 };
